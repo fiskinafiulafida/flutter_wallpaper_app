935d1e7d098666fab0c7cdbac696d8fdd6ab43d0
fiskinafiulafida <fiskinafiul@gmail.com>
2022-04-03 23:52:44 +0700

    home 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wallpaper Flutter',
+      title: 'WallpaperHub',
       // debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,55 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_wallpaper/data/data.dart';
+import 'package:tugas_wallpaper/models/categories_model.dart';
 import 'package:tugas_wallpaper/widgets/widgets.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  get searchController => null;
+
+  List<CategoriesModel> categories = new List;
+
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: brandName(),
         elevation: 0.0,
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xfff5f8fd),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        margin: EdgeInsets.symmetric(horizontal: 24),
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Row(
+        child: Column(
           children: <Widget>[
-            Expanded(
-                child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                  hintText: "search wallpapers", border: InputBorder.none),
-            )),
-            InkWell(
-                onTap: () {
-                  if (searchController.text != "") {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SearchView(
-                                  search: searchController.text,
-                                )));
-                  }
-                },
-                child: Container(child: Icon(Icons.search)))
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search Wallpaper",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                itemCount: categories.length,
+                shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return CategoriesTile(
+                    title: categories[index].categoriesName,
+                    imgUrl: categories[index],
+                  );
+                }),
+            )
           ],
         ),
       ),
     );
   }
+}
 
-  SearchView({search}) {}
+class CategoriesTile extends StatelessWidget {
+  final String imgUrls, title;
+
+  CategoriesTile(@required this.title,@required this.imgUrls);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Image.network(imgUrls),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              child: Text(title),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
