8a6a5f225ac3dd912d00c831de9a04be66d5a0cf
fiskinafiulafida <fiskinafiul@gmail.com>
2022-04-04 23:09:46 +0700

    wallpaper

--- lib/models/wallpaper_model.dart
+++ lib/models/wallpaper_model.dart
@@ -1,5 +1,39 @@
 class WallpaperModel {
   String? photographer;
   String? photographer_url;
-  String? photographer_id;
+  int? photographer_id;
+
+  SrcModel? src;
+
+  WallpaperModel({
+    this.src,
+    this.photographer,
+    this.photographer_id,
+    this.photographer_url,
+  });
+
+  factory WallpaperModel.fromMap(Map<String, dynamic> jsonData) {
+    return WallpaperModel(
+        // src: jsonData["src"],
+        src: SrcModel.fromMap(jsonData["src"]),
+        photographer_url: jsonData["photographer_url"],
+        photographer_id: jsonData["photographer_id"],
+        photographer: jsonData["photographer"]);
+  }
+}
+
+class SrcModel {
+  String? original;
+  String? small;
+  String? portrait;
+
+  SrcModel({this.portrait, this.original, this.small});
+
+  factory SrcModel.fromMap(Map<String, dynamic> jsonData) {
+    return SrcModel(
+      original: jsonData["original"],
+      small: jsonData["small"],
+      portrait: jsonData["portrait"],
+    );
+  }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:tugas_wallpaper/data/data.dart';
 import 'package:tugas_wallpaper/models/categories_model.dart';
+import 'package:tugas_wallpaper/models/wallpaper_model.dart';
 import 'package:tugas_wallpaper/widgets/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,24 +13,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List<CategoriesModel> categories = [];
+  List<WallpaperModel> wallpapers = [];
 
-  getTrendingWallpaper() async  {
-    var response = await http.get("https://api.pexels.com/v1/curated?per_page=15&page=1",
-          headers: {"Authorization": apiKEY});
+  getTrendingWallpaper() async {
+    var response = await http.get(
+        Uri.parse("https://api.pexels.com/v1/curated?per_page=15&page=1"),
+        headers: {"Authorization": apiKEY});
 
-          // print(response.body.toString());
+    // print(response.body.toString());
 
     Map<String, dynamic> jsonData = jsonDecode(response.body);
-    jsonData["photos"].forEach((element){
+    jsonData["photos"].forEach((element) {
       // print(element);
-
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpapers.add(wallpaperModel);
     });
+
+    setState(() {});
   }
 
   @override
   void initState() {
+    getTrendingWallpaper();
     categories = getCategories();
     super.initState();
   }
@@ -71,16 +78,17 @@ class _HomeState extends State<Home> {
             Container(
               height: 80,
               child: ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                itemCount: categories.length,
-                shrinkWrap: true,
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  itemCount: categories.length,
+                  shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index){
-                  return CategoriesTile(
-                    title: categories[index].categoriesName,
-                    imgUrl: categories[index]imgUrl.toString(),
-                  );
-                }),
+                  itemBuilder: (context, index) {
+                    // wallpapers[index].src.portrait
+                    return CategoriesTile(
+                      title: categories[index].categoriesName,
+                      imgUrl: categories[index].imgUrl,
+                    );
+                  }),
             )
           ],
         ),
@@ -92,7 +100,7 @@ class _HomeState extends State<Home> {
 class CategoriesTile extends StatelessWidget {
   final String imgUrls, title;
 
-  CategoriesTile(@required this.title,@required this.imgUrls);
+  CategoriesTile(@required this.title, @required this.imgUrls);
 
   @override
   Widget build(BuildContext context) {
@@ -101,18 +109,26 @@ class CategoriesTile extends StatelessWidget {
       child: Stack(
         children: <Widget>[
           ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(imgUrls,height: 50,
-              width: 100,
-              fit: BoxFit.cover,)),
-            Container(
-              color: Colors.black26,
-              height: 50,
-              width: 100,
-              alignment: Alignment.center,
-              child: Text(title, style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),),
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                imgUrls,
+                height: 50,
+                width: 100,
+                fit: BoxFit.cover,
+              )),
+          Container(
+            color: Colors.black26,
+            height: 50,
+            width: 100,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16),
             ),
+          ),
         ],
       ),
     );
